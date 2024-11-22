c1108a7ad49796b8633b9bbf73ed4b59f6a8a902
Евгений <listopadov_94@mail.ru>
2024-11-22 23:18:00 +0300

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,8 @@
+volume_of_completed_tasks = 12
+hours_spent = 1.5
+python_developer_from_0_to_Middle = "Python"
+time_for_one_task= hours_spent/ volume_of_completed_tasks
+print("Курс:"+python_developer_from_0_to_Middle,
+      ",всего задач:", volume_of_completed_tasks ,
+      ",затрачено часов:", hours_spent ,
+      ",среднее время выполнения" , time_for_one_task , "часа")
