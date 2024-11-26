afb801d7da93188f06db863ddb1bb44bf80b48b6
Евгений <listopadov_94@mail.ru>
2024-11-26 19:58:24 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,12 @@
+my_dict = {"Tom":1994,"Bob":2000,"Timur":2023}
+print(my_dict)
+print(my_dict["Bob"])
+print(my_dict.get("David"))
+my_dict.update({"Antonina":1064,"Max":1999})
+del my_dict["Bob"]
+print(my_dict)
+my_set = {1,2,3,2,3,"Toshka"}
+print(my_set)
+my_set.add((7,66,55))
+my_set.add(9.6)
+print(my_set)
