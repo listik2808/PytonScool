8fef6810c11846a8f40ae3775c48906936e9b525
Евгений <listopadov_94@mail.ru>
2024-11-28 23:41:01 +0300

    Update module_1_6.py
    refactoring

--- module_1_6.py
+++ module_1_6.py
@@ -3,10 +3,10 @@ print(my_dict)
 print(my_dict["Bob"])
 print(my_dict.get("David"))
 my_dict.update({"Antonina":1064,"Max":1999})
-del my_dict["Bob"]
-print(my_dict)
+print(my_dict.pop("Bob"))
 my_set = {1,2,3,2,3,"Toshka"}
 print(my_set)
 my_set.add((7,66,55))
 my_set.add(9.6)
+my_set.remove(1)
 print(my_set)
