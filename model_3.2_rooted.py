612ab01e15df2ce7d3850813a55df2c5f2eea610
Евгений <listopadov_94@mail.ru>
2024-12-10 19:49:11 +0300

    Update model_3.2_rooted.py
    убрал не нужные строки

--- model_3.2_rooted.py
+++ model_3.2_rooted.py
@@ -1,10 +1,3 @@
-#def sort_word(text,root):
- #   str(root).count()
-  #  for j in text:
-   #     if j == root
-
-
-
 def single_root_words(root_word,*other_words):
     same_words =[]
     count = 0
@@ -13,7 +6,6 @@ def single_root_words(root_word,*other_words):
         if count > 0:
             same_words.append(i)
             count = 0
-            print(i,"---Добавили слова---")
     return same_words
 
 result = single_root_words("кот","Котенок","Котичка","Котик","Кошка","Котераптор","роБоКото")
