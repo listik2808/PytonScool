4052bdc4f1191e0d0e00e0bfb1664d78a2e38439
Евгений <listopadov_94@mail.ru>
2024-12-19 21:41:16 +0300

    Update model_3_recursive.py

--- model_3_recursive.py
+++ model_3_recursive.py
@@ -1,11 +1,12 @@
 def get_multiplied_digits(number):
-    str_number = str(number)
-    if len(str_number) > 1 :
-        first = int(str_number[0:1])
-        print(first,"__")
-        return first * get_multiplied_digits(int(str_number[1:]))
+    str_value = str(number)
+    first = int(str_value[0])
+
+    if len(str_value) > 1 :
+        return first * get_multiplied_digits(int(str_value[1:]))
     else:
-        first = int(str_number[0:1])
-        return first
-result = get_multiplied_digits(40203)
-print(result)
+        return first if first != 0 else 1
+result1 = get_multiplied_digits(402030)
+result2 = get_multiplied_digits(40203)
+print(result1)
+print(result2)
